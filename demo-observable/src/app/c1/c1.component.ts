395c8b1fbb5691a6d9f0d26877e78bc9ed790075
Ashwin-Joshy <ashwinkaliyar@gmail.com>
2022-10-16 11:09:09 +0530

    Services updating

--- demo-observable/src/app/c1/c1.component.ts
+++ demo-observable/src/app/c1/c1.component.ts
@@ -9,11 +9,22 @@ import { Observable, observable } from 'rxjs';
 export class C1Component implements OnInit {
   myObservable= new Observable( res=>{
     console.log('observable');
-    
+    res.next("10")
+    res.next("20")
+    res.next("30")
+    res.next("40")
+    res.next("50")
+    res.next("60")
+    res.next("70")
+    res.next("80")
+    res.next("90")
   } )
   constructor() { }
 
   ngOnInit(): void {
+    this.myObservable.subscribe((value)=>{
+      console.log(value);
+    })
   }
 
 }
